ec49a6727af2b33749023269ed76ea1f7ca15fe1
jSSjin <jerat004@gmail.com>
2025-10-30 03:52:10 +0700

    generate unique world id

--- createWorld.js
+++ createWorld.js
@@ -3,12 +3,21 @@ const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 const TABLE = process.env.DDB_TABLE;
-
+const { randomUUID } = require('crypto');
 exports.handler = async (event) => {
   try {
     const body = JSON.parse(event.body || '{}');
     // expect body: { worldId: 'world-123.zip' OR worldId: 'world-123', displayName: 'My World', uploader: 'jirat', s3Key: 'worlds/..' }
     const worldId = body.worldId || (body.s3Key && body.s3Key.split('/').pop());
+    const uniqueSuffix = randomUUID().substring(0, 8);
+    if (!worldId.endsWith('.zip')) {
+      worldId = `${worldId}-${uniqueSuffix}.zip`;
+    } else {
+      const baseName = worldId.replace(/\.zip$/, '');
+      worldId = `${baseName}-${uniqueSuffix}.zip`;
+    }
+    
+    
     const item = {
       worldId,
       displayName: body.displayName || worldId,
